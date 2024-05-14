2171e84c9b342980f70977f71db048644ae0d8bc
mannylopez <manuel.est.lopez@gmail.com>
2024-05-14 12:52:23 -0700

    Add Logger with custom log function

--- TinyMoonApp/MoonViewModel.swift
+++ TinyMoonApp/MoonViewModel.swift
@@ -1,13 +1,15 @@
 // Created by manny_lopez on 5/13/24.
 
 import Foundation
+import os
 import SwiftUI
 import TinyMoon
 
 class MoonViewModel: ObservableObject {
-  var moon: Moon
+  @Published var moon: Moon
 
   init(date: Date = Date()) {
     self.moon = TinyMoon().calculateMoonPhase(date)
+    Logger.log(event: "MoonViewModel initialized")
   }
 }
